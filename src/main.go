d448095089636c81f6a3767232be066459c6ca9d
Saravanan Dayalan <dayalsaravanan@gmail.com>
2022-09-28 17:18:34 +0530

    update time with clock widget

--- src/main.go
+++ src/main.go
@@ -3,13 +3,31 @@ package main
 import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/widget"
+	"time"
 )
+
+func updateTime(clock *widget.Label) {
+	formatted := time.Now().Format("The time is: 03:04:05")
+	clock.SetText(formatted)
+}
 
 func main() {
 	a := app.New()
-	w := a.NewWindow("Hello World") // window title
+	w := a.NewWindow("Clock") // window title
 
 	text := widget.NewLabel("Hello World!") // window content
 	w.SetContent(text)                      // passing window content
+
+	clock := widget.NewLabel(" ")
+	updateTime(clock)
+	w.SetContent(clock)
+
+	// go routine for time update
+	go func() {
+		for range time.Tick(time.Second) {
+			updateTime(clock)
+		}
+	}()
+
 	w.ShowAndRun()
 }
